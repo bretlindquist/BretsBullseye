9a429e4909ba16551f71880873b93fe3344bfc8a
bretlindquist <bretlindquist@gmail.com>
2024-01-23 18:19:51 +0900

    Initial Commit

---
+++ bullseyebuild2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  bullseyebuild2
+//
+//  Created by Bret Allan Lindquist on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bullseyebuild2/bullseyebuild2App.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyebuild2App.swift
+//  bullseyebuild2
+//
+//  Created by Bret Allan Lindquist on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct bullseyebuild2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
